cbc72acc9f81b5c86d2ba2ba5a3303bcc5584f70
Denis Chernykh <forgamesandforum@yandex.ru>
2025-01-23 15:36:18 +0500

    Updates

--- src/types.ts
+++
@@ -1,9 +0,0 @@
-export type Task = {
-  id: number;
-  text: string;
-  isCompleted: boolean;
-  subTask: Task[];
-  isVisible?: boolean;
-};
-
-export type Tasks = Task[];
--- src/ui/Header.tsx
+++
@@ -1,9 +0,0 @@
-function Header({ children }: { children: React.ReactNode }) {
-  return (
-    <header className="flex justify-center gap-4 bg-orange-400 py-7">
-      {children}
-    </header>
-  );
-}
-
-export default Header;
